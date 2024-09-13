fb17141b7c0822fb7982d6ddab3a26081211c1d7
sibelozlem.gul <sibelozlem.gul@sabancidx.com>
2024-09-13 13:32:00 +0300

    end

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,75 +1,76 @@
 import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut
-  } from 'firebase/auth';
-  import { defineStore } from 'pinia';
-  import { auth } from '../js/firebase';
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { useNotesStore } from './NotesStore';
-  
-  export const useAuthStore = defineStore('authStore', () => {
-    const user = ref({});
-    const router = useRouter();
-    const notesStore = useNotesStore();
-    const init = () => {
-      onAuthStateChanged(auth, (userDetails) => {
-        if (userDetails) {
-          const uid = userDetails.uid;
-          user.value = { email: userDetails.email, uid };
-          router.push({ name: 'notes' });
-          notesStore.getNotes();
-        } else {
-          user.value = {};
-          router.replace({ name: 'auth' });
-        }
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth';
+import { defineStore } from 'pinia';
+import { auth } from '../js/firebase';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useNotesStore } from './NotesStore';
+
+export const useAuthStore = defineStore('authStore', () => {
+  const user = ref({});
+  const router = useRouter();
+  const notesStore = useNotesStore();
+  const init = () => {
+    onAuthStateChanged(auth, (userDetails) => {
+      if (userDetails) {
+        const uid = userDetails.uid;
+        user.value = { email: userDetails.email, uid };
+        router.push({ name: 'notes' });
+        notesStore.getNotes();
+      } else {
+        user.value = {};
+        router.replace({ name: 'auth' });
+        notesStore.clearNotes();
+      }
+    });
+  };
+  const registerUser = (credentials) => {
+    createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // ..
       });
-    };
-    const registerUser = (credentials) => {
-      createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          console.log(user);
-          // ...
-        })
-        .catch((error) => {
-          console.log(error.message);
-          // ..
-        });
-    };
-  
-    const loginUser = (credentials) => {
-      signInWithEmailAndPassword(auth, credentials.email, credentials.password)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          console.log(user);
-          // ...
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    };
-  
-    const logoutUser = () => {
-      signOut(auth)
-        .then(() => {
-          console.log('loggedout');
-          // Sign-out successful.
-        })
-        .catch((error) => {
-          // An error happened.
-        });
-    };
-  
-    return {
-      registerUser,
-      loginUser,
-      logoutUser,
-      init,
-      user
-    };
-  });
+  };
+
+  const loginUser = (credentials) => {
+    signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const logoutUser = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('loggedout');
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return {
+    registerUser,
+    loginUser,
+    logoutUser,
+    init,
+    user
+  };
+});
